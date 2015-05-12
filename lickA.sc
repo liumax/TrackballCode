26c705d85f556b386ff4109ec573af0a18cab30c
liumax <liumax@users.noreply.github.com>
2015-05-11 21:51:16 -0700

    These are functional code for driving licking task via statescript. Only has three stateValues for licking stages.

---
+++ lickA.sc
@@ -0,0 +1,42 @@
+%This is the sc file
+%This has only one sound (port 2)
+%This has lickometer at port 3, solenoid at port 4
+
+int rewLength % CHANGE THIS IN BLOCKS
+int soundRewDel % CHANGE THIS EVERY TRIAL
+int baitDur
+int bait1 = 0
+int soundDur
+int preDelay = 1000
+int trialState
+int itiDur
+
+function 1
+    disp('Initiating trial')
+    trialState = 3
+    do in itiDur
+        trialState = 1
+        do in preDelay
+            trialState = 2
+            portout[2] = 1 % sound on
+            disp('SoundOn')
+            do in soundDur
+                portout[2] = 0 % sound off
+                disp('SoundOff')
+            end
+            do in soundRewDel
+                trialState = 3
+                disp('Reward Delivered')
+                portout[4] = 1
+                    do in rewLength
+                        portout[4] = 0
+                        disp(rewLength)
+                    end
+            end
+        end
+    end
+end;
+
+callback portin[3] up % lickometer activated
+    disp(trialState)
+end;
---
+++ lickACallback.m
@@ -0,0 +1,85 @@
+%This is the Matlab Callback
+
+function lickACallback(newLine)
+
+global scQtHistory; %multipurpose place to store processed event history
+global scQtControllerOutput; %the text output from the microcontroller 
+global scQtCallBackHandle; %the handle to the function called for every new event
+global scQtInitiated; %the callback function should set this to 1 once all user variables are set
+
+global scQtUserData;
+
+if (scQtInitiated == 0)
+    lickAScript;
+    scQtInitiated = 1;
+    newLine = 'start next trial';
+end
+
+if (~isempty(strfind(newLine,'SoundOff')))
+    scQtUserData.trial = scQtUserData.trial + 1;
+    sendScQtControlMessage(['disp(''Trial = ',num2str(scQtUserData.trial),''')']);
+    sendScQtControlMessage(['itiDur = ',num2str(scQtUserData.master(scQtUserData.trial,2)-1000)]); %-1000 is fudge to allow trial states to work
+    sendScQtControlMessage(['soundRewDel = ',num2str(scQtUserData.master(scQtUserData.trial,3))]);
+    sendScQtControlMessage(['rewLength = ',num2str(scQtUserData.master(scQtUserData.trial,1))]);
+    sendScQtControlMessage('trigger(1)');
+end
+
+if (~isempty(strfind(newLine,'trialState')))
+    findSpaces=find(newLine == ' ');
+    stateValue=str2double(newLine(findSpaces(end)+1:end));
+    disp(stateValue)
+    if stateValue == 1
+        scQtUserData.master(scQtUserData.trial,6)=scQtUserData.master(scQtUserData.trial,6)+1;
+%         disp '1'
+        disp(scQtUserData.master(scQtUserData.trial,6))
+    elseif stateValue == 2
+        scQtUserData.master(scQtUserData.trial,7)=scQtUserData.master(scQtUserData.trial,7)+1;
+%         disp '2'
+        disp(scQtUserData.master(scQtUserData.trial,7))
+    elseif stateValue == 3
+        scQtUserData.master(scQtUserData.trial,8)=scQtUserData.master(scQtUserData.trial,8)+1;
+%         disp '3'
+        disp(scQtUserData.master(scQtUserData.trial,8))
+    end
+    disp 'State Value Updated'
+end
+
+if (~isempty(strfind(newLine,'SoundOff'))) && scQtUserData.trial>1
+    if ~isfield(scQtUserData,'updateFig')
+        disp('resetting updateFig');
+        scQtUserData.updateFig = -1;
+    end
+    if ~ishandle(scQtUserData.updateFig)
+        scQtUserData.updateFig = figure('color','w');
+        scQtUserData.corrAx = subplot(2,1,1,'parent',scQtUserData.updateFig);
+        scQtUserData.durAx = subplot(2,1,2,'parent',scQtUserData.updateFig);
+        hold(scQtUserData.corrAx,'on');
+        hold(scQtUserData.durAx,'on');
+        ylabel(scQtUserData.corrAx,'Licks');
+        ylabel(scQtUserData.durAx,'Reward Dur (ms)');
+    end
+
+    cla(scQtUserData.corrAx);
+    
+    title(scQtUserData.corrAx,[...
+        'Total trials ', num2str(scQtUserData.trial),...
+        ' Black = prelick',' Blue = Anticipatory', ' Green = post']), ...
+    % Plot pre-licks
+    plot(1:scQtUserData.trial,scQtUserData.master(1:scQtUserData.trial,6),'linewidth',3,...
+        'color','k','parent',scQtUserData.corrAx);
+    % Plot anticipatory licks
+    plot(1:scQtUserData.trial,scQtUserData.master(1:scQtUserData.trial,7),'linewidth',3,...
+        'color','b','parent',scQtUserData.corrAx);
+    %Plot post-delivery licks
+    plot(1:scQtUserData.trial,scQtUserData.master(1:scQtUserData.trial,8),'linewidth',3,...
+        'color','g','parent',scQtUserData.corrAx);
+
+    % Plot reward size
+    plot(1:scQtUserData.trial,scQtUserData.master(1:scQtUserData.trial,1),'color','k','linewidth',3,...
+        'parent',scQtUserData.durAx);
+
+    set(scQtUserData.corrAx,'ylim',[0 15],'ytick',0:1:15,'ygrid','on');
+    set(scQtUserData.durAx,'ygrid','on');
+end
+
+end
