174cff8d536e395d03f45d697f6897990847eb76
liumax <liumax@users.noreply.github.com>
2015-06-10 17:10:11 -0700

    quick code to run code together in batch format. note that maxTrialVariableAnalysis only runs on text files coming after 4/30 (starting 5/1)

---
+++ trialVariableBatch.m
@@ -0,0 +1,17 @@
+clear all
+
+%Note: ls only outputs a matrix of m x n in WINDOWS NOT IN MACINTOSH
+filelist=ls('*.txt');
+[m n]=size(filelist);
+
+
+for i=1:m
+    fname=filelist(i,:);
+    [trialStates, portStates, trialParams] = maxTrialVariables(fname);
+    [cleanLicks] = maxTrialVariableAnalysis(trialStates,trialParams,portStates);
+end
+
+
+
+
+   
