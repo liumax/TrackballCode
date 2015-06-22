e06138b8ed402c0e09dbb8db76d66e2e87cf0c8f
liumax <liumax@users.noreply.github.com>
2015-06-22 11:38:04 -0700

    adjusted so that updates more often, stops displaying a lot of variables, and is cleaner. Also added fudge factors for speed and time to make axes nicer.

--- encoderCallback.m
+++ encoderCallback.m
@@ -19,10 +19,8 @@ if (~isempty(strfind(newLine,'upA')))
     findSpacer=find(newLine == ' ');
     scQtUserData.velocity(scQtUserData.velCounter,1) = str2double(newLine(1:(findSpacer(1)-1)));
     scQtUserData.velocity(scQtUserData.velCounter,2) = str2double(newLine(findSpacer(end)+1:end));
-    x=scQtUserData.velocity(scQtUserData.velCounter,1)
-    y=scQtUserData.velocity(scQtUserData.velCounter,2)
-    scQtUserData.velCounter = scQtUserData.velCounter + 1
-    scQtUserData.plotCounter = scQtUserData.plotCounter + 1
+    scQtUserData.velCounter = scQtUserData.velCounter + 1;
+    scQtUserData.plotCounter = scQtUserData.plotCounter + 1;
 end
 
 if scQtUserData.plotCounter == 10
@@ -32,27 +30,23 @@ if scQtUserData.plotCounter == 10
         scQtUserData.updateFig = -1;
     end
     
-    disp('got here')
-    
     if ~ishandle(scQtUserData.updateFig) %This is to set the basis for all the plots!
         scQtUserData.updateFig = figure('color','w');
         scQtUserData.velAx = subplot(2,1,1,'parent',scQtUserData.updateFig);
         hold(scQtUserData.velAx,'on');
+        xlabel(scQtUserData.velAx,'Time (s)');
         ylabel(scQtUserData.velAx,'Speed (cm/s)');
     end
     
-    disp('and here')
-    
     cla(scQtUserData.velAx); %This clears lickAx. This is because there are constantly adjustments to lickAx
-    
-    disp('finished clearing')
+
     %currently can only update the following trial, since figure updates at
     %a time that isnt conducive to this?
 %     scQtUserData.velStart(scQtUserData.trial) = find(scQtUserData.velocity(:,1)<scQtUserData.master(scQtUserData.trial,10)-5000);
 %     scQtUserData.velEnd(scQtUserData.trial) = find(scQtUserData.velocity(:,1)>scQtUserData.master(scQtUserData.trial,10)+2000);
     
     title(scQtUserData.velAx,'Mouse Speed (cm/s)'), ...
-    plot(scQtUserData.velocity(:,1),scQtUserData.velocity(:,2),'color','k','linewidth',2,...
+    plot((scQtUserData.velocity(:,1)-scQtUserData.velocity(1,1))/1000,scQtUserData.velocity(:,2)*2*0.1,'color','k','linewidth',2,...
     'parent',scQtUserData.velAx);
 
     set(scQtUserData.velAx,'ygrid','on');
