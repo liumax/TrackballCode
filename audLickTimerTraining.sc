8b1f3007f34d2ad8c06d12656a81e24ac3a33d65
liumax <liumax@users.noreply.github.com>
2015-10-29 16:29:06 -0700

    audLickTimerTraining: Test code that should run training in which mice hear 500 ms tones, have 2 second window from tone onset to lick. If lick within 3 seconds of original planned start time, then automatically trigger delay of additional 3 seconds. Also triggers display of a light for duration equal to reward time to serve as warning of excessive licking. UNTESTED

---
+++ audLickTimerTraining.sc
@@ -0,0 +1,128 @@
+%This is the sc file
+%port 1 is lickometer
+%port 2 is reward solenoid
+%ports 3-4 are sounds. 3 is low tone, 4 is high tone. 
+%port 5 is warning light telling mouse they are licking in between
+
+int cueID %determines whether to play high or low tones
+int cueDur %determines how long in ms cue lasts
+int itiDur %duration of intertrial interval
+int rewDur %reward duration
+int rewWin %reward window (should be 2 seconds)
+int lickTicker = 0 %this is self-updating counter of licks
+int noLick %this is period over which no licking is allowed
+
+int rewSwitch = 0
+
+callback portin[1] up
+    disp('Lick Detected')
+    lickTicker = lickTicker + 1
+    do in noLick
+        lickTicker = lickTicker - 1
+    end
+    if rewSwitch == 1 do
+        rewSwitch = 0
+        disp('Reward Delivered')
+        portout[2] = 1
+        do in rewDur
+            portout[2] = 0
+            do in itiDur
+                disp('TriggerMatlab')
+            end
+        end
+    end
+end;
+
+function 2
+    do in noLick
+        if lickTicker > 0 do
+            disp('Bad Licks')
+            portout[5] = 1
+            do in rewDur
+                portout[5] = 0
+            end
+            trigger(2)
+        else if lickTicker == 0 do
+            disp('Initiating Reward Trial')
+            disp('Cue On')
+            if cueID == 0 do
+                portout[3] = 1
+                rewSwitch = 1
+                do in cueDur
+                    portout[3] = 0
+                end
+                do in rewWin
+                    if rewSwitch == 1 do
+                        rewSwitch = 0
+                        do in itiDur
+                            disp('TriggerMatlab')
+                        end
+                    end
+                end
+            end
+            if cueID == 1 do
+                portout[4] = 1
+                rewSwitch = 1
+                do in cueDur
+                    portout[4] = 0
+                end
+                do in rewWin
+                    if rewSwitch == 1 do
+                        rewSwitch = 0
+                        do in itiDur
+                            disp('TriggerMatlab')
+                        end
+                    end
+                end
+            end
+        end
+    end
+end;
+
+
+function 1
+    if lickTicker > 0 do
+        disp('Bad Licks')
+        portout[5] = 1
+        do in rewDur
+            portout[5] = 0
+        end
+        trigger(2)
+    else if lickTicker == 0 do
+        disp('Initiating Reward Trial')
+        disp('Cue On')
+        if cueID == 0 do
+            portout[3] = 1
+            rewSwitch = 1
+            do in cueDur
+                portout[3] = 0
+            end
+            do in rewWin
+                if rewSwitch == 1 do
+                    rewSwitch = 0
+                    do in itiDur
+                        disp('TriggerMatlab')
+                    end
+                end
+            end
+        end
+        if cueID == 1 do
+            portout[4] = 1
+            rewSwitch = 1
+            do in cueDur
+                portout[4] = 0
+            end
+            do in rewWin
+                if rewSwitch == 1 do
+                    rewSwitch = 0
+                    do in itiDur
+                        disp('TriggerMatlab')
+                    end
+                end
+            end
+        end
+    end
+end;
+
+
+
---
+++ audLickTimerTrainingScript.m
@@ -0,0 +1,109 @@
+%This is the Matlab Script
+
+global scQtUserData;
+          
+% UI prompt:
+prompt = {'Mouse ID:',...  
+    'Reward (msec):',...       
+    'Tone (0=lo,1=hi)',...
+    'Number of Trials:',...           
+    'Weight:',...           
+    'sessionID:',...        
+    'Notes:'}; %the bracket is to end the prompt     
+dlg_title = 'auditoryLickOperantTraining:';
+num_lines=1;
+def={'','50','','400','','1',''};
+answer = inputdlg(prompt,dlg_title,num_lines,def);
+pause(2); % need to pause for microcontroller or things break!
+
+% Connect to Microcontroller and store MetaData:
+% setComVal; % Call script to set com port string for this computer
+% sHandle = scConnect(comValStr,@pokeE_Callback);
+% pause(1);
+
+i=1;
+t = clock;
+rand('seed',sum(round(clock)));
+scQtUserData.mouseID = answer{i};i=i+1;
+scQtUserData.rewSize = str2num(answer{i});i=i+1;
+scQtUserData.cueID = str2num(answer{i});i=i+1;
+scQtUserData.totalTrials = str2num(answer{i});i=i+1;
+scQtUserData.weight = answer{i};i=i+1;
+scQtUserData.taskID = 'auditoryLickOperantTraining';
+scQtUserData.sessionID = answer{i};i=i+1;
+scQtUserData.notes = answer{i};i=i+1;
+
+
+%hardcoded values
+scQtUserData.minITI = 7000;
+scQtUserData.maxITI = 12000;
+scQtUserData.cueDur = 500;%duration of cue
+scQtUserData.rewWin = 2000; %how long the animal has to lick in response to cue
+scQtUserData.histLim = [-2,4.9];%edges of the histogram
+scQtUserData.noLick = 3000;%period where licks are not permitted
+
+%Placeholders
+scQtUserData.lickHolder = 1;
+
+%Date/Time
+scQtUserData.date = date;
+scQtUserData.time = strcat(num2str(t(4)),':',num2str(t(5)));
+
+% my additional fields:
+scQtUserData.trial = 0; % keep track of trial number
+scQtUserData.tripSwitch = 0; %triggers end of trials at set number of trials
+scQtUserData.binSize = 100; %msec per bin for rasters/histograms
+scQtUserData.graphAxes = [scQtUserData.histLim(1):scQtUserData.binSize/1000:scQtUserData.histLim(2)];
+
+pause(0.2);
+
+sendScQtControlMessage(['disp(''Mouse ID:', scQtUserData.mouseID,''')']);
+sendScQtControlMessage(['disp(''rewSize:', num2str(scQtUserData.rewSize),''')']);
+sendScQtControlMessage(['disp(''cueID:', num2str(scQtUserData.cueID),''')']);
+sendScQtControlMessage(['disp(''cueDur:', num2str(scQtUserData.cueDur),''')']);
+sendScQtControlMessage(['disp(''rewWin:', num2str(scQtUserData.rewWin),''')']);
+sendScQtControlMessage(['disp(''noLick:', num2str(scQtUserData.noLick),''')']);
+sendScQtControlMessage(['disp(''totalTrials:', num2str(scQtUserData.totalTrials),''')']);
+sendScQtControlMessage(['disp(''minITI:', num2str(scQtUserData.minITI),''')']);
+sendScQtControlMessage(['disp(''maxITI:', num2str(scQtUserData.maxITI),''')']);
+sendScQtControlMessage(['disp(''weight:', scQtUserData.weight,''')']);
+sendScQtControlMessage(['disp(''taskID:', scQtUserData.taskID,''')']);
+sendScQtControlMessage(['disp(''date:', scQtUserData.date,''')']);
+sendScQtControlMessage(['disp(''time:', scQtUserData.time,''')']);
+sendScQtControlMessage(['disp(''sessionID:', scQtUserData.sessionID,''')']);
+sendScQtControlMessage(['disp(''notes:', scQtUserData.notes,''')']);
+
+pause(1) %Need to put all my timings in before this stuff
+
+%calculates ITIs
+k = 2.5;
+p = (1-exp(-k))*rand(scQtUserData.totalTrials,1);
+tau = (scQtUserData.maxITI-scQtUserData.minITI)/k;
+x = round(scQtUserData.minITI + (-log(1-p))*tau); 
+scQtUserData.itiTime = x;
+
+
+%These are temporary placeholders, which get wiped every trial
+scQtUserData.LickTime = zeros (1000,1);
+
+%These are storage for rasters
+
+scQtUserData.rewRast = zeros (10000,2);
+
+%These are placekeeper for rasters
+
+scQtUserData.rewHolder = 1;
+
+%These are storage for histograms
+scQtUserData.rewHist = zeros (length(scQtUserData.graphAxes),1);%%% PROBLEM HERE???
+
+%These are holding event times for plotting
+scQtUserData.cueTime = zeros(scQtUserData.totalTrials,1);
+scQtUserData.rewTime = zeros(scQtUserData.totalTrials,1);
+
+sendScQtControlMessage(['rewDur=',num2str(scQtUserData.rewSize)]);
+sendScQtControlMessage(['cueDur=',num2str(scQtUserData.cueDur)]);
+sendScQtControlMessage(['rewWin=',num2str(scQtUserData.rewWin)]);
+sendScQtControlMessage(['cueID=',num2str(scQtUserData.cueID)]);
+sendScQtControlMessage(['noLick=',num2str(scQtUserData.noLick)]);
+sendScQtControlMessage(['disp(''StartSession'')']);
