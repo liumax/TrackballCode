5233f98dfa99800702026baf47234dbd92b40948
unknown <KreitzerLab@scottowent3610.gladstone.internal>
2016-02-18 16:38:03 -0800

    TrodestoMatclust: made adjustments so trodespath gets read in. this is for extracting continuous data

--- TrodestoMatclust.m
+++ TrodestoMatclust.m
@@ -15,7 +15,10 @@ createAllMatclustFiles
 % %this will extract LFP data
 % extractLFPBinaryFiles(fileName, 0);
 
-% %this will extract continuous data. as of 160216 doesnt work. 
+%this will extract continuous data. as of 160216 doesnt work. 
 % !"C:\Trodes\exportLFP" -rec  160129_ML150108A_L17_3000_toneFinder.rec -userefs 0
-% contDataCommand = ['!"',fullfile(trodesPath,'exportLFP'),'"', ' -rec ',fileName,'.rec',...
-%     ' -usespikefilter 0 -lowpass -1 -highpass -1 -userefs 0 -outputrate 30000'];
+trodesPath = which('trodes_path_placeholder.m');
+trodesPath = fileparts(trodesPath);
+
+contDataCommand = ['!"',fullfile(trodesPath,'exportLFP'),'"', ' -rec ',fileName,'.rec',...
+    ' -usespikefilters 0 -lowpass -1 -highpass -1 -userefs 0 -outputrate 30000'];
