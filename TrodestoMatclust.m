2b7400fa56cc37826eeb97404a203e0cf1c9ec05
unknown <KreitzerLab@scottowent3610.gladstone.internal>
2016-04-15 14:00:10 -0700

    TrodestoMatclust: made adjustments so it changes directories, makes directories, and moves all analysis files out into those directories.

--- TrodestoMatclust.m
+++ TrodestoMatclust.m
@@ -1,7 +1,7 @@
 
 %this is the filename for the .rec file. Please put it in the same dir as
 %the config file if you decide to do post-hoc referencing etc.
-fileName = '160225_ML160218A_L12_2500_fullTuning';
+fileName = '160413_ML160218C_R17_2266_whiteNoise';
 
 %this extracts timestamps
 extractTimeBinaryFile(fileName)
@@ -17,6 +17,37 @@ extractLFPBinaryFiles(fileName, 0);
 
 %this will extract DIO data (inputs and outputs)
 extractDioBinaryFiles(fileName);
+
+%finds all files related to the recording
+dirStruct = dir; %generates directory
+dirNames = {dirStruct.name}; %pulls file/folder names
+allMatches = strfind(dirNames,fileName); %finds things matching target file name
+allMatches = find(~cellfun(@isempty,allMatches)==1); %converts empty cells to logical and finds where it is true
+dirConf = find(arrayfun(@(y)all(y.isdir==1),dirStruct)==1); %finds all things that are directories
+moveDir = intersect(allMatches,dirConf);
+
+%generates directory to put everything into!
+mkdir(fileName(1:end-2)); %makes directory
+for i = 1:size(moveDir,1) %moves all folders in!!
+    movefile(dirNames{moveDir(i)},fileName(1:end-2));
+end
+
+currDir = pwd; %finds current path
+newDir = strcat(currDir,'\',fileName(1:end-2)); %generates path for new directory
+cd(newDir)
+%finds directory, identifies matclust folder!
+dirStruct = dir;
+dirNames = {dirStruct.name};
+allMatches = strfind(dirNames,'matclust');
+allMatches = find(~cellfun(@isempty,allMatches)>0);
+%finds path for matclust folder
+currDir = pwd;
+newDir = strcat(currDir,'\',dirNames{allMatches});
+%changes directory to matclust directory
+cd(newDir)
+%open matclust
+matclust
+
 
 % %this will extract continuous data. as of 160216 doesnt work. 
 % % !"C:\Trodes\exportLFP" -rec  160129_ML150108A_L17_3000_toneFinder.rec -userefs 0
