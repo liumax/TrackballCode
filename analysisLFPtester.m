79c301705baf1e7ae8cbe2821cbc2bd17a107b93
unknown <KreitzerLab@scottowent3610.gladstone.internal>
2016-05-01 13:15:39 -0700

    analysisLFPtester: added code to print out PDF! Also adjusted for purely frequency based analysis

--- analysisLFPtester.m
+++ analysisLFPtester.m
@@ -1,8 +1,8 @@
 %this code is to link LFP with timing of tone inputs. 
-viewWindow = [-0.5,0.5];
+lfpWindow = [-0.5,0.5];
 
-lfp = readTrodesExtractedDataFile('160405_ML160218B_R17_2372_fullTune.LFP_nt1ch1.dat');
-dio = readTrodesExtractedDataFile('160405_ML160218B_R17_2372_fullTune.dio_D1.dat');
+lfp = readTrodesExtractedDataFile('160429_bathTestingLFP.LFP_nt1ch1.dat');
+dio = readTrodesExtractedDataFile('timingTester.dio_D1.dat');
 %counts number of LFP samples
 lfpSamples = size(lfp.fields.data,1);
 %makes LFP time points based on # of samples and decimation
@@ -15,12 +15,12 @@ dioHolder(:,2) = dio.fields(2).data;
 %eliminates non- upswing datapoints
 dioHolder = dioHolder(dioHolder(:,2) == 1);
 %calculates number of samples in the viewing window
-viewSamples = (viewWindow(2)-viewWindow(1))*lfp.clockrate/lfp.decimation;
+viewSamples = (lfpWindow(2)-lfpWindow(1))*lfp.clockrate/lfp.decimation;
 
 %holds all LFP traces
 lfpHolder = zeros(size(dioHolder,1),viewSamples);
 for i = 1:size(dioHolder,1)
-    finder = find(lfpTimes>dioHolder(i)+viewWindow(1)*lfp.clockrate,1);
+    finder = find(lfpTimes>dioHolder(i)+lfpWindow(1)*lfp.clockrate,1);
     lfpHolder(i,:) = lfpSignals(finder:finder+viewSamples-1);
 end
 
@@ -28,25 +28,41 @@ tester = mean(lfpHolder);
 plot(tester);
 
 master(:,1) = soundData.Frequencies;
-master(:,2) = soundData.dBs;
+% master(:,2) = soundData.dBs;
 
 uniqueFreqs = unique(master(:,1));
-uniqueDBs = unique(master(:,2));
+% uniqueDBs = unique(master(:,2));
 trials = soundData.ToneRepetitions;
 
-freqAmpHolder = cell(size(uniqueFreqs,1),size(uniqueDBs,1));
+% freqAmpHolder = cell(size(uniqueFreqs,1),size(uniqueDBs,1));
+freqAmpHolder = cell(size(uniqueFreqs,1),1);
+
+% for i = 1:size(uniqueFreqs,1)
+%     for j = 1:size(uniqueDBs,1)
+%         trialFinder = intersect(find(master(:,1) == uniqueFreqs(i)),...
+%             find(master(:,2) == uniqueDBs(j)));
+%         freqAmpHolder{i,j} = lfpHolder(trialFinder,:);
+%     end
+% end
 
 for i = 1:size(uniqueFreqs,1)
-    for j = 1:size(uniqueDBs,1)
-        trialFinder = intersect(find(master(:,1) == uniqueFreqs(i)),...
-            find(master(:,2) == uniqueDBs(j)));
-        freqAmpHolder{i,j} = lfpHolder(trialFinder,:);
-    end
+    trialFinder = find(master(:,1) == uniqueFreqs(i));
+    freqAmpHolder{i} = lfpHolder(trialFinder,:);
 end
 
 %this is to plot all mean traces by frequency
-figure
+% figure
+% hold on
+% for i =length(uniqueFreqs)
+% plot(mean(freqAmpHolder{i}),'color',rand(3,1))
+% end
+
+h = figure;
 hold on
-for i =1:13
-plot(mean(freqAmpHolder{i,6}),'color',rand(3,1))
+for i =1:length(uniqueFreqs)
+plot(mean(freqAmpHolder{i}),'color',rand(3,1))
 end
+set(h,'Units','Inches');
+pos = get(h,'Position');
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(h,'filename','-dpdf','-r0')
