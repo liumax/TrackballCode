85af3a590587f7022e0d02a6ea15f9afc32d2478
Max Liu <liumax@gmail.com>
2016-06-09 10:32:26 -0700

    functionPairingAverageRate: made adjustments since I'm now separating spikes out initially before this code ever runs.

--- functionPairingAverageRate.m
+++ functionPairingAverageRate.m
@@ -1,19 +1,18 @@
 %This function is meant to calculate average firing rate off of a denoted
-%period of time, which will be given by a 2 element array. Assumed sampling
-%rate is 30,000 Hz for recording data.
+%period of time, which will be given by a 2 element array. spikeTimes
+%should be the structured array containing spike times from the baseline
+%period. Since time is in ms, there is a 1000 fudge factor. 
 
-function [] = functionPairingAverageRate(masterStruct,truncatedNames,...
-    spikeStruct,timesBaseline);
+function [masterStruct] = functionPairingAverageRate(masterStruct,truncatedNames,...
+    spikeStruct,spikeTimes);
 averageFiringRate = cell(size(truncatedNames,2),1);
 for i = 1:size(truncatedNames,2)
-    spikes = spikeStruct.(truncatedNames{i}).SpikeTimes;
     clusters = spikeStruct.(truncatedNames{i}).Clusters;
     averageFireHolder = zeros(clusters,1);
     for j = 1:clusters
-        modSpikes = spikes{j};
-        modSpikes = modSpikes(modSpikes<timesBaseline(2));
-        initialTime = timesBaseline(2) - timesBaseline(1);
-        averageFireHolder(j) = size(modSpikes,1)/(initialTime/30000);
+        numSpikes = size(spikeTimes{j},1);
+        initialTime = (masterStruct.TimePeriods.Baseline(2) - masterStruct.TimePeriods.Baseline(1))*1000;
+        averageFireHolder(j) = numSpikes/initialTime;
     end
     averageFiringRate{i} = averageFireHolder;
 end
