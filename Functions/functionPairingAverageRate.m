a302171f89b261de8adee9999691aa61daf04e34
Max Liu <liumax@gmail.com>
2016-06-10 18:10:35 -0700

    functionPairingAverageFiringRate: finds average firing rate for all periods. This will allow tracking of drift in average firing rate, which could affect results.

--- Functions/functionPairingAverageRate.m
+++ Functions/functionPairingAverageRate.m
@@ -3,18 +3,33 @@
 %should be the structured array containing spike times from the baseline
 %period. 
 
-function [masterStruct] = functionPairingAverageRate(masterStruct,truncatedNames);
-
+function [masterStruct] = functionPairingAverageRate(masterStruct,truncatedNames,...
+    spikeNames,names);
+%pulls average firing rate from baseline period.
 for i = 1:size(truncatedNames,2)
     spikeTimes = masterStruct.(truncatedNames{i}).BaselineSpikes;
     clusters = masterStruct.(truncatedNames{i}).Clusters;
     averageFireHolder = zeros(clusters,1);
     for j = 1:clusters
         numSpikes = size(spikeTimes{j},1);
-        initialTime = (masterStruct.TimePeriods.Baseline(2) - masterStruct.TimePeriods.Baseline(1));
-        averageFireHolder(j) = numSpikes/initialTime;
+        timePeriod = (masterStruct.TimePeriods.Baseline(2) - masterStruct.TimePeriods.Baseline(1));
+        averageFireHolder(j) = numSpikes/timePeriod;
     end
     masterStruct.(truncatedNames{i}).AverageFiringRates = averageFireHolder;
+end
+%pulls average firing rate from all other periods.
+for i = 1:size(truncatedNames,2)
+    clusters = masterStruct.(truncatedNames{i}).Clusters;
+    averageFireHolder = zeros(clusters,1);
+    for j = 1:size(spikeNames,1)
+        spikeTimes = masterStruct.(truncatedNames{i}).(spikeNames{j});
+        for k = 1:clusters
+            numSpikes = size(spikeTimes{k},1);
+            timePeriod = (masterStruct.TimePeriods.(names{j})(2) - masterStruct.TimePeriods.(names{j})(1));
+            averageFireHolder(k) = numSpikes/timePeriod;
+        end
+        masterStruct.(truncatedNames{i}).Averages.(names{j}) = averageFireHolder;
+    end
 end
 
 end
