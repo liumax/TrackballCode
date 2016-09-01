eede2c2ec60c18104de62e16358eaa7cca1bac92
unknown <KreitzerLab@scottowent3610.gladstone.internal>
2016-09-01 09:33:10 -0700

    soundGeneratorPulsedTonesWithLaser: adjusted so that control can have independent frequency/amplitude, and so that this information is recorded.

--- AuditoryStimulusGeneration/soundGeneratorPulsedTonesWithLaser.m
+++ AuditoryStimulusGeneration/soundGeneratorPulsedTonesWithLaser.m
@@ -10,7 +10,7 @@ fs = 192000; %sampling frequency in Hz
 prePause = 0.1; %pause in seconds before tone delivery
 
 %control over timing of blocks of tone pips
-totalReps = 50; %number of repetitions of tone blocks
+totalReps = 10; %number of repetitions of tone blocks
 totalITI = 10; %ITI of tone blocks in seconds. DOES NOT INCLUDE TONE DUR
 
 %control over parameters of individual tone pips
@@ -19,6 +19,12 @@ toneFreq = 'white';
 toneDB = 60;%amplitude in dB with 100 set as max with driver at no attenuation, speakers set to max
 maxDB = 100;%DONT ADJUST
 toneAmp= 10^-((maxDB-toneDB)/20);
+
+%control for tone pips 
+controlFreq = 8000;
+% controlFreq = 'white';
+controlDB = 100;
+controlAmp = 10^-((maxDB-controlDB)/20);
 
 indivReps = 15; %number of tone pips per presentation
 indivITI = 0.2; %ITI of tone pips in seconds. This includes the tone duration.
@@ -60,6 +66,14 @@ if maxDB - toneDB < 0
     error('DB value greater than maximum')
 end
 
+if laserSwitch ~= 1 & laserSwitch ~= 0
+    error('Laser Mode is Invalid')
+end
+
+if laserTiming > indivReps
+    error('Laser Timing is Invalid')
+end
+
 %%start prepping sound pip file. This code makes the small tone pip sound and TTL.
 
 %first, make the amplitude envelope
@@ -97,6 +111,27 @@ elseif ~ischar(toneFreq)
     tonePip = tonePip .* rampProfile;
 end
 
+%fill control tone space with white noise or tone
+if ischar(controlFreq) & strfind(controlFreq,'white')
+    %generate white gaussian noise of correct size
+    y = wgn(fs*toneDur,1,0);
+    %filters white gaussian noise (courtesy of RYAN MORRILL)
+    Wn = 4e3/(0.5*fs); % pass above 3.9 kHz 160121 Adjusted to 4kHz
+    n = 1000; % 1000th order filter (slower? but 100-order was too low)
+    %160121 Adjusted from 1000 to 100000, this gets greater attenuation, which is
+    %necessary for my higher volumes. This achieves 90dB attenuation for all
+    %frequencies below 4kHz.
+    %160121 Returned to 1000. Increasing n to 100000 causes failure of
+    %filtfilt. Checked by doing fft(X,n) with n = number of samples, seems to
+    %filter out everything below 4kHz very effectively. 
+    b = fir1(n, Wn, 'high'); 
+    audio_data = filtfilt(b,1,y);
+    controlPip = audio_data.*rampProfile;
+elseif ~ischar(controlFreq)
+    controlPip = sin(2*pi*(controlFreq/fs)*(1:tonePipDur))';
+    controlPip = controlPip .* rampProfile;
+end
+
 %%now put the small pips together! This will also incorporate laser. This
 %%generates a two column matrix that incorporates sound (column 1) and ttl
 %%(column 2)
@@ -118,7 +153,7 @@ end
 controlBlock = zeros(blockDur,2);
 
 for i = 1:indivReps
-    controlBlock(startTimes(i):startTimes(i)-1+tonePipDur,1) = tonePip;
+    controlBlock(startTimes(i):startTimes(i)-1+tonePipDur,1) = controlPip;
     controlBlock(startTimes(i):startTimes(i)-1+tonePipDur,2) = pipTTL;
 end
 
@@ -153,9 +188,13 @@ else
 end
 
 soundData = struct;
-soundData.Frequency = toneFreq;
-soundData.DB = toneDB;
-soundData.Amplitude = toneAmp;
+soundData.TargetFrequency = toneFreq;
+soundData.TargetDB = toneDB;
+soundData.TargetAmplitude = toneAmp;
+soundData.ControlFrequency = controlFreq;
+soundData.ControlDB = controlDB;
+soundData.ControlAmplitude = controlAmp;
+
 soundData.BlockRepetitions = totalReps;
 soundData.BlockITI = totalITI;
 soundData.PipRepetitions = indivReps;
