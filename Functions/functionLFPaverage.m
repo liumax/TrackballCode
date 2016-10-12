8dca64e1b4e3916412539c3050392aa74019589b
Max Liu <liumax@gmail.com>
2016-10-12 10:24:20 -0700

    functionLFPaverage: made adjustments to code to reduce amount of text output, and changed indexes to reduce chances for future problems with those.

--- Functions/functionLFPaverage.m
+++ Functions/functionLFPaverage.m
@@ -1,4 +1,4 @@
-function [s] = functionLFPaverage(master, lfpWindow, matclustStruct,homeFolder,fileName, uniqueFreqs, uniqueDBs, numFreqs, numDBs);
+function [lfpStruct] = functionLFPaverage(master, lfpWindow, s,homeFolder,fileName, uniqueFreqs, uniqueDBs, numFreqs, numDBs);
 
 %%
 %now I will extract LFP information!
@@ -25,11 +25,11 @@ numLFPs = size(lfpFiles,2);
 %this generates the set of colors I want to use
 colorArray = zeros(60,3);
 colorArray(1,:) = [0,0,1];
-for i = 1:29
-    colorArray(i+1,:) = [0,(i)/29,(29-i)/29];
+for lfpInd1 = 1:29
+    colorArray(lfpInd1+1,:) = [0,(lfpInd1)/29,(29-lfpInd1)/29];
 end
-for i = 1:30
-    colorArray(i+30,:) = [(i)/30,(30-i)/30,0];
+for lfpInd1 = 1:30
+    colorArray(lfpInd1+30,:) = [(lfpInd1)/30,(30-lfpInd1)/30,0];
 end 
 %this spaces out my frequencies to tile the color space I have defined
 spacer = size(colorArray,1)/size(uniqueFreqs,1);
@@ -50,14 +50,14 @@ numTrials = size(master(:,1),1);
 targetTimesFreqs = zeros(numTrials/numFreqs,numFreqs);
 targetTimesDBs = zeros(numTrials/numDBs,numDBs);
 
-for i = 1:numFreqs
-    freqFinder = find(master(:,2) == uniqueFreqs(i));
-    targetTimesFreqs(:,i) = master(freqFinder,1);
+for lfpInd1 = 1:numFreqs
+    freqFinder = find(master(:,2) == uniqueFreqs(lfpInd1));
+    targetTimesFreqs(:,lfpInd1) = master(freqFinder,1);
 end
 
-for i = 1:numDBs
-    dbFinder = find(master(:,3) == uniqueDBs(i));
-    targetTimesDBs(:,i) = master(dbFinder,1);
+for lfpInd1 = 1:numDBs
+    dbFinder = find(master(:,3) == uniqueDBs(lfpInd1));
+    targetTimesDBs(:,lfpInd1) = master(dbFinder,1);
 end
 
 %the next step is to extract the specific window I'm interested in from the
@@ -69,9 +69,9 @@ viewSamples = round((lfpWindow(2)-lfpWindow(1))*lfp.clockrate/lfp.decimation);
 
 lfpAverageFreqs = zeros(numLFPs,numFreqs,viewSamples);
 
-for i = 1:numLFPs
+for lfpInd1 = 1:numLFPs
         %opens LFP file
-    lfp = readTrodesExtractedDataFile(lfpFiles{i});
+    lfp = readTrodesExtractedDataFile(lfpFiles{lfpInd1});
     %counts number of LFP samples
     lfpSamples = size(lfp.fields.data,1);
     %makes LFP time points based on # of samples and decimation
@@ -82,25 +82,22 @@ for i = 1:numLFPs
     tempHolder = zeros(numTrials/numFreqs,viewSamples);
     %goes through all trials of a given frequency, extracts LFP signals
     %and places into tempHolder
-    for j = 1:numFreqs
-        for k = 1:numTrials/numFreqs
-            finder = find(lfpTimes > targetTimesFreqs(k,j)+lfpWindow(1),1);
-            tempHolder(k,:) = lfpSignals(finder:finder + viewSamples - 1);
+    for lfpInd2 = 1:numFreqs
+        for lfpInd3 = 1:numTrials/numFreqs
+            finder = find(lfpTimes > targetTimesFreqs(lfpInd3,lfpInd2)+lfpWindow(1),1);
+            tempHolder(lfpInd3,:) = lfpSignals(finder:finder + viewSamples - 1);
         end
-        lfpAverageFreqs(i,j,:) = mean(tempHolder);
-        disp('Frequency')
-        disp(j)
+        lfpAverageFreqs(lfpInd1,lfpInd2,:) = mean(tempHolder);
     end
-    disp('electrode')
-    disp(i)
+    disp(strcat('Averaged Electrode ',lfpInd1,' By Frequency'))
 end
 disp('Done with LFPs by Freq')
 
 lfpAverageDBs = zeros(numLFPs,numDBs,viewSamples);
 
-for i = 1:numLFPs
+for lfpInd1 = 1:numLFPs
     %opens LFP file
-    lfp = readTrodesExtractedDataFile(lfpFiles{i});
+    lfp = readTrodesExtractedDataFile(lfpFiles{lfpInd1});
     %counts number of LFP samples
     lfpSamples = size(lfp.fields.data,1);
     %makes LFP time points based on # of samples and decimation
@@ -111,17 +108,14 @@ for i = 1:numLFPs
     tempHolder = zeros(numTrials/numDBs,viewSamples);
     %goes through all trials of a given frequency, extracts LFP signals
     %and places into tempHolder
-    for j = 1:numDBs
-        for k = 1:numTrials/numDBs
-            finder = find(lfpTimes > targetTimesDBs(k,j)+lfpWindow(1),1);
-            tempHolder(k,:) = lfpSignals(finder:finder + viewSamples - 1);
+    for lfpInd2 = 1:numDBs
+        for lfpInd3 = 1:numTrials/numDBs
+            finder = find(lfpTimes > targetTimesDBs(lfpInd3,lfpInd2)+lfpWindow(1),1);
+            tempHolder(lfpInd3,:) = lfpSignals(finder:finder + viewSamples - 1);
         end
-        lfpAverageDBs(i,j,:) = mean(tempHolder);
-        disp('Decibel')
-        disp(j)
+        lfpAverageDBs(lfpInd1,lfpInd2,:) = mean(tempHolder);
     end
-    disp('electrode')
-    disp(i)
+    disp(strcat('Averaged Electrode ',lfpInd1,' By DB'))
 end
 
 disp('Done with LFPs by DB')
@@ -139,27 +133,27 @@ maxLFPdb = max(max(max(lfpAverageDBs)));
 %lines that indicate tone duration
 totalLFPWindow = lfpWindow(2)-lfpWindow(1);
 lfpZero = abs(lfpWindow(1))*viewSamples/totalLFPWindow;
-toneEnd = abs(matclustStruct.ToneDur)*viewSamples/totalLFPWindow;
+toneEnd = abs(s.SoundData.ToneDuration)*viewSamples/totalLFPWindow;
 
 %save everything to a dummy structured array
-s.MeansByFreq = lfpAverageFreqs;
-s.MeansByDB = lfpAverageDBs;
-s.AnalysisWindow = lfpWindow;
-s.ToneStartTime = lfpZero;
-s.ToneEndTime = toneEnd;
+lfpStruct.MeansByFreq = lfpAverageFreqs;
+lfpStruct.MeansByDB = lfpAverageDBs;
+lfpStruct.AnalysisWindow = lfpWindow;
+lfpStruct.ToneStartTime = lfpZero;
+lfpStruct.ToneEndTime = toneEnd;
 
 %Plotting!!
 
 %generates cell array of frequency names for use in legend
 freqNameHolder = cell(1,numFreqs);
-for i =1:numFreqs
-    freqNameHolder{i} = num2str(uniqueFreqs(i));
+for lfpInd1 =1:numFreqs
+    freqNameHolder{lfpInd1} = num2str(uniqueFreqs(lfpInd1));
 end
 
 %generates cell array of DB names for use in legend
 dbNameHolder = cell(1,numDBs);
-for i =1:numDBs
-    dbNameHolder{i} = num2str(uniqueDBs(i));
+for lfpInd1 =1:numDBs
+    dbNameHolder{lfpInd1} = num2str(uniqueDBs(lfpInd1));
 end
 
 h = figure;
@@ -172,21 +166,20 @@ descr = {'LFP By Frequency and DB';
     'Window (s):';
     lfpWindow;
     'Tone Duration(s):';
-    matclustStruct.ToneDur};
+    s.SoundData.ToneDuration};
 set(mTextBox,'String',descr);
 set(mTextBox,'Units','normalized');
 set(mTextBox,'Position',[0.36,0.74,0.3,0.2])
 
 %plots by frequency
-for i = 1:numLFPs
-    subplot(numLFPs,3,1+(3*(i-1)))
+for lfpInd1 = 1:numLFPs
+    subplot(numLFPs,3,1+(3*(lfpInd1-1)))
     plot([lfpZero lfpZero],[minLFPfreq maxLFPfreq],'k');
     hold on
     plot([lfpZero+toneEnd lfpZero+toneEnd],[minLFPfreq maxLFPfreq],'k')
-    hold on
-    for j = 1:numFreqs
-        plot(squeeze(lfpAverageFreqs(i,j,:)),'color',plotColorsFreq(j,:));
-        freqTicker(j) = plot(squeeze(lfpAverageFreqs(i,j,:)),'color',plotColorsFreq(j,:));
+    for lfpInd2 = 1:numFreqs
+        plot(squeeze(lfpAverageFreqs(lfpInd1,lfpInd2,:)),'color',plotColorsFreq(lfpInd2,:));
+        freqTicker(lfpInd2) = plot(squeeze(lfpAverageFreqs(lfpInd1,lfpInd2,:)),'color',plotColorsFreq(lfpInd2,:));
     end
     
     xlim([0 viewSamples])
@@ -195,14 +188,14 @@ for i = 1:numLFPs
     sub_pos = get(gca,'position'); % get subplot axis position
     set(gca,'position',sub_pos.*[1 1 1 1.4]) % stretch its width and height
     
-    subplot(numLFPs,3,3+(3*(i-1)))
+    subplot(numLFPs,3,3+(3*(lfpInd1-1)))
     plot([lfpZero lfpZero],[minLFPdb maxLFPdb],'k');
     hold on
     plot([lfpZero+toneEnd lfpZero+toneEnd],[minLFPdb maxLFPdb],'k')
     hold on
-    for j = 1:numDBs
-        plot(squeeze(lfpAverageDBs(i,j,:)),'color',plotColorsDB(j,:));
-        dbTicker(j) = plot(squeeze(lfpAverageDBs(i,j,:)),'color',plotColorsDB(j,:));
+    for lfpInd2 = 1:numDBs
+        plot(squeeze(lfpAverageDBs(lfpInd1,lfpInd2,:)),'color',plotColorsDB(lfpInd2,:));
+        dbTicker(lfpInd2) = plot(squeeze(lfpAverageDBs(lfpInd1,lfpInd2,:)),'color',plotColorsDB(lfpInd2,:));
     end
     
     xlim([0 viewSamples])
