8e46f05439a50232e7bb3e95d1bf09f35799c39d
Max Liu <liumax@gmail.com>
2016-11-30 14:12:45 -0800

    analysisTuningSelectionTool: code meant to go through a set of tuning curve data that hasnt been processed by other analysis and allows manual selection of tuned units. Saves all tuned units in a structured array.

---
+++ analysisTuningSelectionTool.m
@@ -0,0 +1,49 @@
+%This code is meant to go through, run analysis scripts, and then show
+%figures to allow for approval/disapproval. 
+
+%Establishes the home folder as the base
+masterFolder = pwd;
+masterDir = dir;
+masterDir = {masterDir.name};
+%this code here is to remove folders reported in dir like "." or ".."
+masterIndex = strfind(masterDir,'ML');
+masterIndex = find(not(cellfun('isempty', masterIndex)));
+%masterFolders is a listing of all folders that I want to target for batch
+%analysis
+masterFolders = masterDir(masterIndex);
+numFolders = size(masterFolders,2);
+
+%establishes master array for storage of data
+
+masterStruct = struct;
+masterStruct.masterNames = cell(0,0);
+masterStruct.TuningDesignation = [];
+
+for masterCount = 1:numFolders
+    %generates text that signifies path to the target folder
+    targetPath = strcat(masterFolder,'\',masterFolders{masterCount});
+    cd(targetPath)
+    %what lists matlab files in the folder. can extract based on file type.
+    fileName = what;
+    fileName = fileName.mat{1};
+    periodFinder = strfind(fileName,'.');
+    fileName = fileName(1:periodFinder-1);
+    [s] = functionBasicTuning(fileName);
+    %opens tool which will allow for selection of tuned units, stores these
+    %units in decisionTuning (1 for tuned, 0 for untuned).
+    cd(masterFolder)
+    [decisionTuning] = functionTuningSelectionTool(s,fileName);
+    
+    %adjust designationNames to include filename
+    desigNames = genvarname(strcat(fileName,s.DesignationName));
+    %store these data! 
+    masterStruct.masterNames = [masterStruct.masterNames,desigNames];
+    masterStruct.TuningDesignation = [masterStruct.TuningDesignation;decisionTuning];
+    %store unit analysis data.
+    for i = 1:length(s.DesignationName);
+        masterStruct.(desigNames{i}) = s.(s.DesignationName{i});
+    end
+    %store sound data.
+    masterStruct.(strcat('SoundData',fileName)) = s.SoundData;
+    
+end
