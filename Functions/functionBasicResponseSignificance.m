a260afcf23e9ed136d49a075662793d4195cea0f
Max Liu <liumax@gmail.com>
2016-12-01 18:52:09 -0800

    functionBasicResponseSignificance: code to calculate significnat responses in tone period. Uses either the baseline period of histogram bins as a distribution, or will calculate one by shuffling all spikes from the entire period in question with 1second bins. Confirm works well.

--- Functions/functionBasicResponseSignificance.m
+++ Functions/functionBasicResponseSignificance.m
@@ -30,59 +30,71 @@
 % baseline period. 
 
 
-function [sigResp] = functionBasicResponseSignificance(s,calcWindow,baselineSpikes,inputRaster,trialNum,rasterWindow);
+function [sigResp] = functionBasicResponseSignificance(s,calcWindow,spikeTimes,alignTimes,trialNum);
+
+baselineSpikes = functionBasicRaster(spikeTimes,alignTimes,s.Parameters.BaselineWindow);
+inputRaster = functionBasicRaster(spikeTimes,alignTimes,calcWindow);
+inputRaster = inputRaster(:,1);
+
+histBin = s.Parameters.histBin;
 
 if length(baselineSpikes) > s.Parameters.minSpikes
-    %% First step is to calculate a bootstrapped baseline distribution
-    %Calculate spike ISIs
-    negDiff = diff(baselineSpikes);
-    %pull number of times to shuffle spikes
-    numShuffle = s.Parameters.numShuffle;
-    %pull number of spikes
-    numSpikes = length(baselineSpikes);
-%     baselineWindow = s.Parameters.BaselineWindow;
+    %% First step is to computer an overall histogram of baseline bins.
+    baselineVector = [s.Parameters.BaselineWindow(1) + s.Parameters.histBin/2:s.Parameters.histBin:s.Parameters.BaselineWindow(2)];
+    baselineHist = hist(baselineSpikes(:,1),baselineVector)/histBin/trialNum;
+    %These bins will serve as the distribution against which we will
+    %compare. Currently, with 0.4 seconds/0.005 sec bins, this should
+    %produce 80 bins.
+    
+%     %Calculate spike ISIs
+%     negDiff = diff(sort(baselineSpikes(:,1)));
+%     %pull number of times to shuffle spikes
+%     numShuffle = s.Parameters.numShuffle;
+%     %pull number of spikes
+%     numSpikes = length(baselineSpikes);
+% %     baselineWindow = s.Parameters.BaselineWindow;
+% 
+%     %allocate array for storage of data
+%     simDataHolder = zeros(numShuffle,numSpikes);
+%     simDataHolder(:,1) = baselineSpikes(1); %seed with first spike time
+% 
+%     for respInd = 1:numShuffle
+%         %generate random permutation of ISIs
+%         randInd = randperm(numSpikes-1);
+%         %add these to the data!
+%         simDataHolder(respInd,2:end) = negDiff(randInd);
+%     end
+%     %sum to generate appropriate values (adding ISIs to times)
+%     simDataHolder = cumsum(simDataHolder,2);
+% 
+%     %process so that produce histogram with appropriate bin size
+%     histBin = s.Parameters.histBin;
+%     baseHistVector = [rasterWindow(1)+histBin/2:histBin:0];
+%     simHist = hist(simDataHolder',baseHistVector);
+% 
+%     %reshape and normalize data so that reporting firing rate in Hz. 
+%     baselineSim = reshape(simHist,[],1)/histBin/trialNum;
 
-    %allocate array for storage of data
-    simDataHolder = zeros(numShuffle,numSpikes);
-    simDataHolder(:,1) = baselineSpikes(1); %seed with first spike time
 
-    for respInd = 1:numShuffle
-        %generate random permutation of ISIs
-        randInd = randperm(numSpikes-1);
-        %add these to the data!
-        simDataHolder(respInd,2:end) = negDiff(randInd);
-    end
-    %sum to generate appropriate values (adding ISIs to times)
-    simDataHolder = cumsum(simDataHolder,2);
-
-    %process so that produce histogram with appropriate bin size
-    histBin = s.Parameters.histBin;
-    baseHistVector = [rasterWindow(1)+histBin/2:histBin:0];
-    simHist = hist(simDataHolder',baseHistVector);
-
-    %reshape and normalize data so that reporting firing rate in Hz. 
-    baselineSim = reshape(simHist,[],1)/histBin/trialNum;
-
-
-    %% Based on bootstrapped distribution, generate positive percentile values
-    %generate a percentile range from the bootstrapped data. 
+    %% Based on  distribution, generate positive percentile values
+    %generate a percentile range from the data. 
     percentileRange = 100.-(s.Parameters.zLimit*100);
 
     %find the values from the distribution that are associated with the given
     %percentiles
     valueRange = zeros(length(percentileRange),1);
+%     simValueRange = zeros(length(percentileRange),1);
 
     for respInd = 1:length(percentileRange)
-        valueRange(respInd) = prctile(baselineSim,percentileRange(respInd));
+        valueRange(respInd) = prctile(baselineHist,percentileRange(respInd));
+%         simValueRange(respInd) = prctile(baselineSim,percentileRange(respInd));
     end
 
-    %% Now we use the bootstrapped distribution and percentiles to calculate positive significant responses
-    %eliminate data points outside of desired calculation window
-    targetData = inputRaster(inputRaster>calcWindow(1) & inputRaster<calcWindow(2));
+    %% Now we use the  distribution and percentiles to calculate positive significant responses
     %generate vector for bin centers for histogram
     targetHistVector = [calcWindow(1) + histBin/2: histBin:calcWindow(2)];
     %calculate histogram
-    targetHist = hist(targetData,targetHistVector);
+    targetHist = hist(inputRaster,targetHistVector);
     targetHist = reshape(targetHist,[],1)/histBin/trialNum;
     %allocate space for information about significance
     targetHist(:,2:length(percentileRange)+1) = zeros;
@@ -95,8 +107,10 @@ if length(baselineSpikes) > s.Parameters.minSpikes
     %% Now look for negative responses
     negRange = s.Parameters.zLimit*100;
     negValues = zeros(length(negRange),1);
+%     simNegValues = zeros(length(negRange),1);
     for respInd = 1:length(percentileRange)
-        negValues(respInd) = prctile(baselineSim,negRange(respInd));
+        negValues(respInd) = prctile(baselineHist,negRange(respInd));
+%         simNegValues(respInd) = prctile(baselineSim,negRange(respInd));
     end
     %allocate space
     targetHist(:,end+1:end+length(percentileRange)) = zeros;
@@ -119,22 +133,74 @@ if length(baselineSpikes) > s.Parameters.minSpikes
     sigResp.SpikeNumber = length(baselineSpikes);
     sigResp.SigSpike = sigSpike;
 else
-    %% If too few spikes, generate histogram with no significant values.
-    histBin = s.Parameters.histBin;
-    %eliminate data points outside of desired calculation window
-    targetData = inputRaster(inputRaster>calcWindow(1) & inputRaster<calcWindow(2));
-    %generate vector for bin centers for histogram
-    targetHistVector = [calcWindow(1) + histBin/2:histBin:calcWindow(2)];
-    %calculate histogram
-    targetHist = hist(targetData,targetHistVector);
-    %allocate space for information about significance
-    targetHist(:,2:length(s.Parameters.zLimit)+1) = zeros;
+    %% If too few spikes, generate baseline firing based on entire spike train
+    allISI = diff(spikeTimes);
+    totalTime = spikeTimes(end) - spikeTimes(1);
+    totalVector = [s.Parameters.BaselineCalcBins/2:s.Parameters.BaselineCalcBins:totalTime];
+    totalHistHolder = zeros(length(totalVector),s.Parameters.numShuffle);
+    for permRep = 1:s.Parameters.numShuffle
+        permInd = randperm(length(allISI));
+        permSpikes = cumsum(allISI(permInd));
+        totalHistHolder(:,permRep) = hist(permSpikes,totalVector);
+    end
+    totalHistHolder = reshape(totalHistHolder,[],1);
     
-    %save data!
+    %now that we have a baseline, lets calculate significance!
+    %% Based on  distribution, generate positive percentile values
+    %generate a percentile range from the data. 
+    percentileRange = 100.-(s.Parameters.zLimit*100);
+
+    %find the values from the distribution that are associated with the given
+    %percentiles
+    valueRange = zeros(length(percentileRange),1);
+%     simValueRange = zeros(length(percentileRange),1);
+
+    for respInd = 1:length(percentileRange)
+        valueRange(respInd) = prctile(totalHistHolder,percentileRange(respInd));
+%         simValueRange(respInd) = prctile(baselineSim,percentileRange(respInd));
+    end
+    
+    %% Now we use the  distribution and percentiles to calculate positive significant responses
+    %generate vector for bin centers for histogram
+    targetHistVector = [calcWindow(1) + histBin/2: histBin:calcWindow(2)];
+    %calculate histogram
+    targetHist = hist(inputRaster,targetHistVector);
+    targetHist = reshape(targetHist,[],1)/histBin/trialNum;
+    %allocate space for information about significance
+    targetHist(:,2:length(percentileRange)+1) = zeros;
+    %find all values above specific thresholds
+    for respInd = 1:length(percentileRange)
+        sigFinder = find(targetHist(:,1)> valueRange(respInd));
+        targetHist(sigFinder,respInd+1) = 1;
+    end
+    
+    %% Now look for negative responses
+    negRange = s.Parameters.zLimit*100;
+    negValues = zeros(length(negRange),1);
+%     simNegValues = zeros(length(negRange),1);
+    for respInd = 1:length(percentileRange)
+        negValues(respInd) = prctile(totalHistHolder,negRange(respInd));
+%         simNegValues(respInd) = prctile(baselineSim,negRange(respInd));
+    end
+    %allocate space
+    targetHist(:,end+1:end+length(percentileRange)) = zeros;
+    %find all values below specific thresholds
+    for respInd = 1:length(percentileRange)
+        sigFinder = find(targetHist(:,1)< negValues(respInd));
+        targetHist(sigFinder,length(percentileRange)+respInd+1) = 1;
+    end
+    
+    if length(find(targetHist(:,2:end)>0))>s.Parameters.minSigSpikes;
+        sigSpike = 1;
+    else
+        sigSpike = 0;
+    end
+    
+    %% save data!
     sigResp.Histogram = targetHist;
     sigResp.Centers = targetHistVector;
     sigResp.Warning = 1; %warning indicates baseline was too low for shuffling.
-    sigResp.SpikeNumber = length(baselineSpikes);
-%     disp('Insufficient Spikes for Shuffling. Not performing analysis')
+    sigResp.SpikeNumber = length(spikeTimes);
+    sigResp.SigSpike = sigSpike;
 end
 end
