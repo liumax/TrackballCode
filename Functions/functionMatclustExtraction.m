a28ef11943c3867789e321939d23eea775dcbde2
max <liumax@gmail.com>
2017-01-27 12:47:37 -0800

    functionMatclustExtraction: Added code functionality to exclude units based on RPV violations or laser aligned violations (indicating artifact).

--- Functions/functionMatclustExtraction.m
+++ Functions/functionMatclustExtraction.m
@@ -10,6 +10,61 @@ function [s, truncatedNames] = functionMatclustExtraction(rpvTime,...
 dupSelectLim = 0.20;
 spikeHolder = cell(0,0);
 expPower = 4;
+rpvPercentCutoff = 5; %percent cutoff for RPVs. in %, so this is actually like 0.05
+% rpvSpikeCutoff = 100; %raw spike number cutoff for RPVs
+
+artifactJitter = 0.0005; %in seconds!
+artifactCutoff = 0.7; %ratio cutoff for laser aligned spikes. 
+
+%first, lets see if there was any laser playback. If there was, this
+%initiates code for looking at laser alignment. 
+
+homeFolder = pwd;
+subFolders = genpath(homeFolder);
+addpath(subFolders)
+subFoldersCell = strsplit(subFolders,';')';
+
+%finds DIO folder, extracts D2 specifically
+[D2FileName] = functionFileFinder(subFoldersCell,'DIO','D2');
+D2FileName = D2FileName{1};
+
+%extracts DIO stuffs!
+[DIO2Data] = readTrodesExtractedDataFile(D2FileName);
+
+%extracts port states and times of changes
+dio2State = double(DIO2Data.fields(2).data);
+dio2Time = double(DIO2Data.fields(1).data)/s.Parameters.SampleRate;
+
+%if there isnt anything coming through, this will only have one value.
+laserSwitch = 0;
+if length(dio2State) > 1
+    laserSwitch = 1;
+    %pulls the times when state goes up/down
+    upTimes = dio2Time(dio2State == 1);
+    downTimes = dio2Time(dio2State == 0);
+
+    %we want the duration of the pulse. Therefore, we need to determine if
+    %upTimes == downTimes. Because the program always begins on a zero, lets
+    %use this a system to eliminate extras. We'll find the first up, and remove
+    %all downs before it
+    firstUp = upTimes(1);
+    downTimes(downTimes<firstUp) = [];
+
+    %check!
+    if length(upTimes) == length(downTimes)
+        disp('Up Times and Down Times Matched')
+    else
+        error(strcat('Mismatched Pulses Up (',num2str(length(upTimes)),') vs Down(',num2str(length(downTimes)),')'))
+    end
+
+    %pulls the duration of the pulse (this should give laser pulse duration
+    dioDiff = downTimes - upTimes;
+    meanDiff = mean(dioDiff);
+    windowArtifact = [-0.1,meanDiff*2];
+    s.LaserData.LaserStartTimes = upTimes;
+    s.LaserData.LaserEndTimes = downTimes;
+    s.LaserData.LaserDuration = meanDiff;
+end
 
 %The code below extracts out the names of the nTrodes and generates both
 %names for nTrode-Cluster combinations, as well as an array for indexing
@@ -34,7 +89,7 @@ for clusterCount = 1:numTrodes
     %file.
     for nameCount = 1:clusterLength
         desigNames{desigCounter} = strcat(truncatedNames{clusterCount},'cluster',num2str(matclustFile.clustattrib.clustersOn(nameCount)));
-        s.(desigNames{desigCounter}) = [];
+        
         desigArray(desigCounter,1) = clusterCount;
         desigArray(desigCounter,2) = nameCount;
         %now I do my conventional stuff for analyzing spike timing
@@ -51,29 +106,87 @@ for clusterCount = 1:numTrodes
         rpvPercent = rpvNumber/totalNumber*100;
         %for graphing purposes, save a small subset of spikes to show RPV
         selectedSpikes = diffSpikes(diffSpikes<clusterWindow(2));
-        %calculate overall firing rate
-        totalTime = matclustFile.clustdata.datarange(2,1)-matclustFile.clustdata.datarange(1,1);
-        overallFiring = totalNumber/totalTime;
-        %calculate average waveform with standard error.
-        %pull waves and squeeze to a 40 x X array
-        targetWaveName = strcat('waves_',truncatedNames{clusterCount},'.mat');
-        waveLoader = open(targetWaveName);
-        %extract relevant waves
-        waveHolder = waveLoader.waves(:,:,clusterSpikes);
-        %pull out average and standard error
-        averageWaveHolder = mean(waveHolder,3);
-        %store into structured array!
-        s.(desigNames{desigCounter}).ISIGraph = selectedSpikes;
-        s.(desigNames{desigCounter}).RPVNumber = rpvNumber;
-        s.(desigNames{desigCounter}).RPVPercent = rpvPercent;
-        s.(desigNames{desigCounter}).TotalSpikeNumber = totalNumber;
-        s.(desigNames{desigCounter}).SpikeTimes = spikeTimes;
-        s.(desigNames{desigCounter}).OverallFiringRate = overallFiring;
-        s.(desigNames{desigCounter}).TotalTimeRecording = totalTime;
-        
-        s.(desigNames{desigCounter}).AverageWaveForms = averageWaveHolder;
-        s.(desigNames{desigCounter}).AllWaves = waveHolder;
-        desigCounter = desigCounter + 1;
+        %now lets remove things with shitty RPVs and/or laser response
+        if laserSwitch == 0
+            if rpvPercent > rpvPercentCutoff
+                disp(strcat('Unit',desigNames{desigCounter},'Fails RPVs with:',num2str(rpvPercent),'% violations out of',num2str(totalNumber),' Spikes'))
+                desigNames{desigCounter} = [];
+                desigArray(desigCounter,:) = [];
+            else
+                %calculate overall firing rate
+                totalTime = matclustFile.clustdata.datarange(2,1)-matclustFile.clustdata.datarange(1,1);
+                overallFiring = totalNumber/totalTime;
+                %calculate average waveform with standard error.
+                %pull waves and squeeze to a 40 x X array
+                targetWaveName = strcat('waves_',truncatedNames{clusterCount},'.mat');
+                waveLoader = open(targetWaveName);
+                %extract relevant waves
+                waveHolder = waveLoader.waves(:,:,clusterSpikes);
+                %pull out average and standard error
+                averageWaveHolder = mean(waveHolder,3);
+                %store into structured array!
+                s.(desigNames{desigCounter}) = [];
+                s.(desigNames{desigCounter}).ISIGraph = selectedSpikes;
+                s.(desigNames{desigCounter}).RPVNumber = rpvNumber;
+                s.(desigNames{desigCounter}).RPVPercent = rpvPercent;
+                s.(desigNames{desigCounter}).TotalSpikeNumber = totalNumber;
+                s.(desigNames{desigCounter}).SpikeTimes = spikeTimes;
+                s.(desigNames{desigCounter}).OverallFiringRate = overallFiring;
+                s.(desigNames{desigCounter}).TotalTimeRecording = totalTime;
+
+                s.(desigNames{desigCounter}).AverageWaveForms = averageWaveHolder;
+                s.(desigNames{desigCounter}).AllWaves = waveHolder;
+                desigCounter = desigCounter + 1;
+            end
+        elseif laserSwitch == 1
+            %find laser aligned spikes
+            [rasters] = functionBasicRaster(spikeTimes,upTimes,windowArtifact);
+            %find all spikes within 0.5 ms window of onset, offset
+            spikesOnset = rasters(rasters(:,1) < 0+artifactJitter & rasters(:,1) > 0-artifactJitter,1);
+            spikesOffset = rasters(rasters(:,1) < meanDiff+artifactJitter & rasters(:,1) > meanDiff-artifactJitter,1);
+
+            numSpikes = length(spikeTimes);
+            propOnset = length(spikesOnset)/numSpikes;
+            propOffset = length(spikesOffset)/numSpikes;
+            if rpvPercent > rpvPercentCutoff
+                disp(strcat('Unit',desigNames{desigCounter},'Fails RPVs with:',num2str(rpvPercent),'% violations out of',num2str(totalNumber),' Spikes'))
+                desigNames{desigCounter} = [];
+                desigArray(desigCounter,:) = [];
+            elseif propOnset > artifactCutoff 
+                disp(strcat('Unit',desigNames{desigCounter},'Fails LaserOnset with:',num2str(propOnset),'laser aligned spikes'))
+                desigNames{desigCounter} = [];
+                desigArray(desigCounter,:) = [];
+            elseif propOffset > artifactCutoff
+                disp(strcat('Unit',desigNames{desigCounter},'Fails LaserOffset with:',num2str(propOffset),'laser aligned spikes'))
+                desigNames{desigCounter} = [];
+                desigArray(desigCounter,:) = [];
+            else
+                %calculate overall firing rate
+                totalTime = matclustFile.clustdata.datarange(2,1)-matclustFile.clustdata.datarange(1,1);
+                overallFiring = totalNumber/totalTime;
+                %calculate average waveform with standard error.
+                %pull waves and squeeze to a 40 x X array
+                targetWaveName = strcat('waves_',truncatedNames{clusterCount},'.mat');
+                waveLoader = open(targetWaveName);
+                %extract relevant waves
+                waveHolder = waveLoader.waves(:,:,clusterSpikes);
+                %pull out average and standard error
+                averageWaveHolder = mean(waveHolder,3);
+                %store into structured array!
+                s.(desigNames{desigCounter}) = [];
+                s.(desigNames{desigCounter}).ISIGraph = selectedSpikes;
+                s.(desigNames{desigCounter}).RPVNumber = rpvNumber;
+                s.(desigNames{desigCounter}).RPVPercent = rpvPercent;
+                s.(desigNames{desigCounter}).TotalSpikeNumber = totalNumber;
+                s.(desigNames{desigCounter}).SpikeTimes = spikeTimes;
+                s.(desigNames{desigCounter}).OverallFiringRate = overallFiring;
+                s.(desigNames{desigCounter}).TotalTimeRecording = totalTime;
+
+                s.(desigNames{desigCounter}).AverageWaveForms = averageWaveHolder;
+                s.(desigNames{desigCounter}).AllWaves = waveHolder;
+                desigCounter = desigCounter + 1;
+            end
+        end
     end
 end
 
