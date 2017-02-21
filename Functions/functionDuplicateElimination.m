de562a8dd72d97633a8b94d292eccb483d8f5469
Max Liu <liumax@gmail.com>
2017-02-21 00:05:41 -0800

    functionDuplicateElimination: appear to have resolved a bug that was appearing with selection of second unit. Replaced names variable with s.DesignationName, and this appears to have resolved the problem. Tested on one dataset.

--- Functions/functionDuplicateElimination.m
+++ Functions/functionDuplicateElimination.m
@@ -19,15 +19,14 @@ lags = [-lagWindow:1:lagWindow];
 
 %pull number of units
 numUnits = length(s.DesignationArray);
-names = s.DesignationName;
 
 %construct spike trains, store in temporary structure
 tmp = struct;
 for tmpCount = 1:numUnits
-    tmp.(names{tmpCount}).SpikeTrain = zeros(round((lastPoint-firstPoint)*30000/downSampFactor),1);
-    tmp.(names{tmpCount}).SpikeTrain(round((s.(names{tmpCount}).SpikeTimes-firstPoint)*30000/downSampFactor)) = 1;
+    tmp.(s.DesignationName{tmpCount}).SpikeTrain = zeros(round((lastPoint-firstPoint)*30000/downSampFactor),1);
+    tmp.(s.DesignationName{tmpCount}).SpikeTrain(round((s.(s.DesignationName{tmpCount}).SpikeTimes-firstPoint)*30000/downSampFactor)) = 1;
     %also create a temporally downsampled list of spike times
-    tmp.(names{tmpCount}).SpikeTimes = round((s.(names{tmpCount}).SpikeTimes-firstPoint)*30000/downSampFactor);
+    tmp.(s.DesignationName{tmpCount}).SpikeTimes = round((s.(s.DesignationName{tmpCount}).SpikeTimes-firstPoint)*30000/downSampFactor);
 end
 
 %figure out what kind of probe I'm using!
@@ -67,7 +66,7 @@ set(hFig, 'Position', [10 80 600 600])
 
 for shnkInd = 1:shanks
     disp(strcat('Analyzing Shank:',num2str(shnkInd)))
-    %find the correct names for the targeted shank
+    %find the correct s.DesignationName for the targeted shank
     indShank = find(ismember(s.DesignationArray(:,1),shankDesig(:,shnkInd)));
     %find the unique trodes represented
     uniqueTrodes = unique(s.DesignationArray(indShank,1));
@@ -88,15 +87,15 @@ for shnkInd = 1:shanks
                 %find the indices for these units
                 indNeighbor = find(ismember(s.DesignationArray(:,1),neighbor));
                 uniqueUnits = find(s.DesignationArray(:,1) == uniqueTrodes(trdInd));
-                disp(strcat('Analyzing:',names{uniqueUnits(unitInd)}))
+                disp(strcat('Analyzing:',s.DesignationName{uniqueUnits(unitInd)}))
                 %first step is gross go over, will basically use the crude
                 %old code I was using to see if there is any chance of
                 %overlap between the two units. 
                 compHolder = zeros(length(indNeighbor),3);
                 for nghbrInd = 1:length(indNeighbor)
-                    compHolder(nghbrInd,1) = length(intersect(tmp.(names{uniqueUnits(unitInd)}).SpikeTimes,tmp.(names{indNeighbor(nghbrInd)}).SpikeTimes));
-                    compHolder(nghbrInd,2) = compHolder(nghbrInd,1)/length(tmp.(names{uniqueUnits(unitInd)}).SpikeTimes);
-                    compHolder(nghbrInd,3) = compHolder(nghbrInd,1)/length(tmp.(names{indNeighbor(nghbrInd)}).SpikeTimes);
+                    compHolder(nghbrInd,1) = length(intersect(tmp.(s.DesignationName{uniqueUnits(unitInd)}).SpikeTimes,tmp.(s.DesignationName{indNeighbor(nghbrInd)}).SpikeTimes));
+                    compHolder(nghbrInd,2) = compHolder(nghbrInd,1)/length(tmp.(s.DesignationName{uniqueUnits(unitInd)}).SpikeTimes);
+                    compHolder(nghbrInd,3) = compHolder(nghbrInd,1)/length(tmp.(s.DesignationName{indNeighbor(nghbrInd)}).SpikeTimes);
                 end
                 %find maximum percentage of spikes shared.
                 compTester = max(compHolder(:,[2:3]),[],2);
@@ -110,54 +109,54 @@ for shnkInd = 1:shanks
                     disp(strcat(num2str(length(compFinder)),' Potential Duplicates Detected, Performing xCorr'))
                     while ~isempty(compFinder)
                         %computer xcorr
-                        [r,~] = xcorr(tmp.(names{uniqueUnits(unitInd)}).SpikeTrain,...
-                            tmp.(names{indNeighbor(compFinder(1))}).SpikeTrain,lagWindow);
+                        [r,~] = xcorr(tmp.(s.DesignationName{uniqueUnits(unitInd)}).SpikeTrain,...
+                            tmp.(s.DesignationName{indNeighbor(compFinder(1))}).SpikeTrain,lagWindow);
                         %% plot to display
                         clf;
                         subplot(3,1,1)
                         plot(lags,r)
                         xlim([-lagWindow,lagWindow])
                         ylim([0 max(r)])
-                        title({strcat('Correlogram of:',names{uniqueUnits(unitInd)},'&',names{indNeighbor(compFinder(1))});...
-                            strcat(names{uniqueUnits(unitInd)},':',num2str(length(tmp.(names{uniqueUnits(unitInd)}).SpikeTimes)),',',...
-                            names{indNeighbor(compFinder(1))},':',num2str(length(tmp.(names{indNeighbor(compFinder(1))}).SpikeTimes)))})
+                        title({strcat('Correlogram of:',s.DesignationName{uniqueUnits(unitInd)},'&',s.DesignationName{indNeighbor(compFinder(1))});...
+                            strcat(s.DesignationName{uniqueUnits(unitInd)},':',num2str(length(tmp.(s.DesignationName{uniqueUnits(unitInd)}).SpikeTimes)),',',...
+                            s.DesignationName{indNeighbor(compFinder(1))},':',num2str(length(tmp.(s.DesignationName{indNeighbor(compFinder(1))}).SpikeTimes)))})
                         subplot(3,1,2)
                         hold on
                         trueMax = 0;
                         trueMin = 0;
-                        numWaves = size(s.(names{uniqueUnits(unitInd)}).AverageWaveForms,2);
+                        numWaves = size(s.(s.DesignationName{uniqueUnits(unitInd)}).AverageWaveForms,2);
                         for m =1:numWaves
-                            plot(s.(names{uniqueUnits(unitInd)}).AverageWaveForms(:,m)-(100*(m-1)),'LineWidth',2)
-                            testMax = max(s.(names{uniqueUnits(unitInd)}).AverageWaveForms(:,m)-(100*(m-1)));
+                            plot(s.(s.DesignationName{uniqueUnits(unitInd)}).AverageWaveForms(:,m)-(100*(m-1)),'LineWidth',2)
+                            testMax = max(s.(s.DesignationName{uniqueUnits(unitInd)}).AverageWaveForms(:,m)-(100*(m-1)));
                             if testMax > trueMax
                                 trueMax = testMax;
                             end
-                            testMin = min(s.(names{uniqueUnits(unitInd)}).AverageWaveForms(:,m)-(100*(m-1)));
+                            testMin = min(s.(s.DesignationName{uniqueUnits(unitInd)}).AverageWaveForms(:,m)-(100*(m-1)));
                             if testMin < trueMin
                                 trueMin = testMin;
                             end
                         end
-                        xlim([0 length(s.(names{uniqueUnits(unitInd)}).AverageWaveForms(:,1))])
+                        xlim([0 length(s.(s.DesignationName{uniqueUnits(unitInd)}).AverageWaveForms(:,1))])
                         ylim([trueMin trueMax])
-                        title(strcat('Waves of:',names{uniqueUnits(unitInd)}))
+                        title(strcat('Waves of:',s.DesignationName{uniqueUnits(unitInd)}))
                         subplot(3,1,3)
                         hold on
                         trueMax = 0;
                         trueMin = 0;
                         for m =1:numWaves
-                            plot(s.(names{indNeighbor(compFinder(1))}).AverageWaveForms(:,m)-(100*(m-1)),'LineWidth',2)
-                            testMax = max(s.(names{indNeighbor(compFinder(1))}).AverageWaveForms(:,m)-(100*(m-1)));
+                            plot(s.(s.DesignationName{indNeighbor(compFinder(1))}).AverageWaveForms(:,m)-(100*(m-1)),'LineWidth',2)
+                            testMax = max(s.(s.DesignationName{indNeighbor(compFinder(1))}).AverageWaveForms(:,m)-(100*(m-1)));
                             if testMax > trueMax
                                 trueMax = testMax;
                             end
-                            testMin = min(s.(names{indNeighbor(compFinder(1))}).AverageWaveForms(:,m)-(100*(m-1)));
+                            testMin = min(s.(s.DesignationName{indNeighbor(compFinder(1))}).AverageWaveForms(:,m)-(100*(m-1)));
                             if testMin < trueMin
                                 trueMin = testMin;
                             end
                         end
-                        xlim([0 length(s.(names{indNeighbor(compFinder(1))}).AverageWaveForms(:,1))])
+                        xlim([0 length(s.(s.DesignationName{indNeighbor(compFinder(1))}).AverageWaveForms(:,1))])
                         ylim([trueMin trueMax])
-                        title(strcat('Waves of:',names{indNeighbor(compFinder(1))}))
+                        title(strcat('Waves of:',s.DesignationName{indNeighbor(compFinder(1))}))
                         %% Ask for input
                         promptCounter = 1; %This is used to run the while loop.
                         whileCounter = 0; %this is the counter that gets updated to exit the loop
@@ -191,14 +190,14 @@ for shnkInd = 1:shanks
                             %will be the deleted unit. The name in the
                             %field will be the unit that triggered the
                             %deletion. 
-                            s.DeletedUnits.(names{indNeighbor(compFinder(1))}) = (names{uniqueUnits(unitInd)});
+                            s.DeletedUnits.(s.DesignationName{indNeighbor(compFinder(1))}) = (s.DesignationName{uniqueUnits(unitInd)});
                             %delete data from second unit from s
-                            s = rmfield(s,(names{indNeighbor(compFinder(1))}));
-                            %remove from s names and designation array
+                            s = rmfield(s,(s.DesignationName{indNeighbor(compFinder(1))}));
+                            %remove from s s.DesignationName and designation array
                             %find the target name
-                            nameInd = find(~cellfun(@isempty,strfind(s.DesignationName,names{indNeighbor(compFinder(1))})));
+                            nameInd = find(~cellfun(@isempty,strfind(s.DesignationName,s.DesignationName{indNeighbor(compFinder(1))})));
                             s.DesignationName(nameInd) = [];
-%                             names(nameInd) = [];
+%                             s.DesignationName(nameInd) = [];
                             s.DesignationArray(nameInd,:) = [];
                             %increment compFinder
                             compFinder(1) = [];
@@ -213,13 +212,13 @@ for shnkInd = 1:shanks
                             %create new variable in s to record unit as
                             %being deleted. Save the unit that was the
                             %reason for deletion
-                            s.DeletedUnits.(names{uniqueUnits(unitInd)}) = (names{indNeighbor(compFinder(1))});
+                            s.DeletedUnits.(s.DesignationName{uniqueUnits(unitInd)}) = (s.DesignationName{indNeighbor(compFinder(1))});
                             %delete data from second unit from s
-                            s = rmfield(s,(names{uniqueUnits(unitInd)}));
-                            %remove from s names and designation array
-                            nameInd = find(~cellfun(@isempty,strfind(s.DesignationName,names{uniqueUnits(unitInd)})));
+                            s = rmfield(s,(s.DesignationName{uniqueUnits(unitInd)}));
+                            %remove from s s.DesignationName and designation array
+                            nameInd = find(~cellfun(@isempty,strfind(s.DesignationName,s.DesignationName{uniqueUnits(unitInd)})));
                             s.DesignationName(nameInd) = [];
-%                             names(nameInd) = [];
+%                             s.DesignationName(nameInd) = [];
                             s.DesignationArray(nameInd,:) = [];
                             %delete compFinder, since the target of
                             %comparison has been removed
