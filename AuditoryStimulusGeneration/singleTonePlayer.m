ae34ae02ddcb25d3acc4538b1678357bd8ab4022
unknown <Kreitzer Lab@mliu-M15W.gladstone.internal>
2017-03-30 10:46:52 -0700

    singleTonePlayer: added code so you can adjust amplitude. functional.

--- AuditoryStimulusGeneration/singleTonePlayer.m
+++ AuditoryStimulusGeneration/singleTonePlayer.m
@@ -1,6 +1,7 @@
 toneReps = 200;
+toneAmp = 0.1;
 toneFreq = 4000;
-toneDur = 0.1;
+toneDur = 5;
 fs = 192000; %sampling frequency in Hz
 L = toneDur*fs; %number of samples at correct sampling frequency
 paddingL = L + fs*1; %adds 0.1 seconds of padding to the end of the tone to ensure things are not cut off.
@@ -22,7 +23,7 @@ ttlSig = zeros(paddingL,1);
 ttlSig(1:2*fs/1000) = 1;
 
 toneWave = sin(2*pi*(toneFreq/fs)*(1:L))';
-finalWave = toneWave.*rampProfile;
+finalWave = toneWave.*rampProfile*toneAmp;
 paddedWave = zeros(paddingL,1);
 paddedWave(1:size(finalWave,1)) = finalWave;
 soundVector = [paddedWave,ttlSig];
