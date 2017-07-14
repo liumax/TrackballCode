49ec1b48dadc5e58cda3b5089244675023e52517
scottowent3610\KreitzerLab <liumax@gmail.com>
2017-07-13 18:53:15 -0700

    testerWrapperPhotometryTuning:  code to repeat photometry tuning analysis

---
+++ testerWrapperPhotometryTuning.m
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+%This will be a wrapper function for the photometry analysis code. 
+
+testNames = what;
+testNames = testNames.mat;
+[findString] = functionCellStringFind(testNames,'Analysis');
+testNames(findString) = [];
+
+[findString] = functionCellStringFind(testNames,'Sound');
+testNames(findString) = [];
+
+numFiles = length(testNames);
+
+for bigInd = 1:numFiles
+    fileName = testNames{bigInd}(1:end-4);
+    [s] = analysisPhotometryTuning(fileName);
+    
+end
