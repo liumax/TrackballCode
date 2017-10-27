5e6bc8c994d00220c0d6ebce7f4b8e405c867096
Max Liu <liumax@gmail.com>
2017-10-27 14:58:24 -0700

    batchAnalysisPavTwoToneCatchFree: batch for pavTwoToneCatchFree. Functional.

---
+++ batchAnalysisPavTwoToneCatchFree.m
@@ -0,0 +1,16 @@
+%This will be a wrapper function for the photometry analysis code. 
+
+testNames = what;
+testNames = testNames.mat;
+[findString] = functionCellStringFind(testNames,'Analysis');
+testNames(findString) = [];
+[findString] = functionCellStringFind(testNames,'Pav');
+testNames = testNames(findString);
+
+numFiles = length(testNames);
+
+for bigInd = 1:numFiles
+    fileName = testNames{bigInd}(1:end-4);
+    [s] = analysisPavTwoToneCatchFree(fileName);
+    s = [];
+end
