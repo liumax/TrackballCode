607af9aeb0bea9e3807ae0298cfd77be50615a9d
scottowent3610\KreitzerLab <liumax@gmail.com>
2018-01-12 20:28:07 -0800

    batchAnalysisTwoTonePavlov: code adjustment to fix issues with detection of TMP files. Believe to be functional.

--- batchAnalysisTwoTonePavlov.m
+++ batchAnalysisTwoTonePavlov.m
@@ -6,11 +6,20 @@ testNames = testNames.mat;
 testNames(findString) = [];
 [findString] = functionCellStringFind(testNames,'Pavlov');
 testNames = testNames(findString);
+[findString] = functionCellStringFind(testNames,'TMP');
+testNames(findString) = [];
 
 numFiles = length(testNames);
-
+badInd = 1
 for bigInd = 1:numFiles
     fileName = testNames{bigInd}(1:end-4);
-    [s] = analysisTwoTonePavlovPhotometry(fileName);
-    s = [];
+    try
+        [s] = analysisTwoTonePavlovPhotometry(fileName);
+        s = [];
+    catch
+        badStore(badInd) = bigInd;
+        badInd = badInd + 1;
+        disp('CODE FAILURE, SAVING FILE')
+        diary off
+    end
 end
