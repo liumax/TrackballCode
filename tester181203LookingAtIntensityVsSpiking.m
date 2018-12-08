a25eaeea5828015e00f9063e097ed71a8a414435
Max Liu <liumax@gmail.com>
2018-12-07 16:03:35 -0800

    testerLookingAtIntensityVsSpiking: adjsuted to add a lot of plotting functions. Confirmed functional.

--- tester181203LookingAtIntensityVsSpiking.m
+++ tester181203LookingAtIntensityVsSpiking.m
@@ -4,7 +4,7 @@ testNames = testNames.mat;
 [findString] = functionCellStringFind(testNames,'Analysis');
 testNames = testNames(findString);
 
-
+tonePeriod = 1;
 numFiles = length(testNames);
 counter = 1;
 for j = 1:numFiles
@@ -16,22 +16,34 @@ for j = 1:numFiles
     sigValStore = [];
     for i = 1:numUnits
         %first, we need to see whether there is a significant response
-        SigVals = s.(s.DesignationName{i}).BinSigVals(:,:,2);
+        SigVals = s.(s.DesignationName{i}).BinSigVals(:,:,tonePeriod);
         %threshold it!
         SigVals(SigVals < sigThresh) = 0;
         SigVals(SigVals >0) = 1;
+        SigValsLaser = s.(s.DesignationName{i}).BinSigVals(:,:,tonePeriod);
+        %threshold it!
+        SigValsLaser(SigValsLaser < sigThresh) = 0;
+        SigValsLaser(SigValsLaser >0) = 1;
         numSigVals = length(find(SigVals == 0));
         sigValStore(i) = numSigVals;
         %now lets use this as a cutoff
         if numSigVals > valThresh
             disp('Significant Crossing!')
+            unitName{counter} = strcat(testNames{j},s.DesignationName{i});
             %now lets find the best frequency based on 70db response.
             [Y,I] = max(s.(s.DesignationName{i}).BinTone(:,9)); %I is best frequency
             if I
-                intensityResponse(:,i) = s.(s.DesignationName{i}).BinTone(I,:);
-                intensityResponseLaser(:,i) = s.(s.DesignationName{i}).BinToneLaser(I,:);
-                bigInt(:,counter) = s.(s.DesignationName{i}).BinTone(I,:);
-                bigIntLaser(:,counter) = s.(s.DesignationName{i}).BinToneLaser(I,:);
+                intensityResponse(:,i) = s.(s.DesignationName{i}).BinDiff(I,:,tonePeriod);
+                intensityResponseLaser(:,i) = s.(s.DesignationName{i}).BinDiffLaser(I,:,tonePeriod);
+                bigInt(:,counter) = s.(s.DesignationName{i}).BinDiff(I,:,tonePeriod);
+                bigIntLaser(:,counter) = s.(s.DesignationName{i}).BinDiffLaser(I,:,tonePeriod);
+                bigSig(:,counter) = SigVals(I,:);
+                bigSigLaser(:,counter) = SigValsLaser(I,:);
+                fullSig(:,:,counter) = SigVals;
+                fullSigTone(:,:,counter) = s.(s.DesignationName{i}).BinSigVals(:,:,2);
+                fullSigLaser(:,:,counter) = SigValsLaser;
+                fullVals(:,:,counter) = s.(s.DesignationName{i}).BinDiff(:,:,tonePeriod);
+                fullValsLaser(:,:,counter) = s.(s.DesignationName{i}).BinDiffLaser(:,:,tonePeriod);
                 cellType(counter) = masterData(i,7);
                 counter = counter + 1;
 %                 figure
@@ -96,6 +108,167 @@ sigVal = 0.05;
 counter = 1;
 slopeStore = [];
 intStore = [];
+valueStore=[];
+for i = 1:length(fsis)
+    [b,bintr,bintjm] = gmregress(bigInt(:,fsis(i)),bigIntLaser(:,fsis(i)),sigVal);
+    %lets also store the values.
+    valueStore(counter:counter + size(bigInt,1) - 1,:) = [bigInt(:,fsis(i)),bigIntLaser(:,fsis(i))];
+    counter = counter + size(bigInt,1);
+    slopeStore(i) = b(2);
+    intStore(i) = b(1);
+end
+
+%now we can generate an average based on the slopes and also based on all
+%the points
+
+avSlopeFSI = nanmean(slopeStore)
+avIntFSI = nanmean(intStore)
+[b,bintr,bintjm] = gmregress(valueStore(:,1),valueStore(:,2),sigVal);
+
+%lets look at average effects for MSNs
+
+% [b,bintr,bintjm] = gmregress(valStore(:,1),valStore(:,2),sigVal);
+sigVal = 0.05;
+counter = 1;
+slopeStore = [];
+intStore = [];
+valueStore=[];
+for i = 1:length(msns)
+    [b,bintr,bintjm] = gmregress(bigInt(:,msns(i)),bigIntLaser(:,msns(i)),sigVal);
+    %lets also store the values.
+    valueStore(counter:counter + size(bigInt,1) - 1,:) = [bigInt(:,msns(i)),bigIntLaser(:,msns(i))];
+    counter = counter + size(bigInt,1);
+    slopeStore(i) = b(2);
+    intStore(i) = b(1);
+end
+
+%now we can generate an average based on the slopes and also based on all
+%the points
+
+avSlopeMSN = nanmean(slopeStore)
+avIntMSN = nanmean(intStore)
+[b,bintr,bintjm] = gmregress(valueStore(:,1),valueStore(:,2),sigVal);
+
+
+%now lets try and do this thing where we find thresholds etc. First thing
+%we need to do with this is clean up the data. 
+fullSigTone(fullSigTone < 0.05) = 0;
+fullSigTone(fullSigTone >0) = 1;
+for i =1:length(fullSig)
+    figure
+    subplot(2,1,1)
+    imagesc(fullSig(:,:,i))
+    colormap('parula')
+    title(unitName{i}, 'Interpreter', 'none')
+    subplot(2,1,2)
+    imagesc(fullSigTone(:,:,i))
+    colormap('parula')
+end
+
+
+
+clear
+
+%% now lets try the same with peak values
+testNames = what;
+testNames = testNames.mat;
+[findString] = functionCellStringFind(testNames,'Analysis');
+testNames = testNames(findString);
+
+
+numFiles = length(testNames);
+counter = 1;
+for j = 1:numFiles
+    load(testNames{j})
+    disp(strcat('Loading-',testNames{j}))
+    numUnits = length(s.DesignationName);
+    sigThresh = 0.05;
+    valThresh = 8;
+    sigValStore = [];
+    for i = 1:numUnits
+        %first, we need to see whether there is a significant response
+        SigVals = s.(s.DesignationName{i}).BinSigVals(:,:,2);
+        %threshold it!
+        SigVals(SigVals < sigThresh) = 0;
+        SigVals(SigVals >0) = 1;
+        numSigVals = length(find(SigVals == 0));
+        sigValStore(i) = numSigVals;
+        %now lets use this as a cutoff
+        if numSigVals > valThresh
+            disp('Significant Crossing!')
+            %now lets find the best frequency based on 70db response.
+            [Y,I] = max(s.(s.DesignationName{i}).PeakMapTone(:,end)); %I is best frequency
+            if I
+                intensityResponse(:,i) = s.(s.DesignationName{i}).PeakMapTone(I,:);
+                intensityResponseLaser(:,i) = s.(s.DesignationName{i}).PeakMapToneLaser(I,:);
+                bigInt(:,counter) = s.(s.DesignationName{i}).PeakMapTone(I,:);
+                bigIntLaser(:,counter) = s.(s.DesignationName{i}).PeakMapToneLaser(I,:);
+                cellType(counter) = masterData(i,7);
+                counter = counter + 1;
+%                 figure
+%                 subplot(2,1,1)
+%                 plot(intensityResponse(:,i),'k')
+%                 hold on
+%                 plot(intensityResponseLaser(:,i) ,'g')
+%                 
+%                 subplot(2,1,2)
+%                 plot(intensityResponseLaser(:,i) - intensityResponse(:,i))
+            end
+        end
+    end
+
+end
+
+fsis = find(cellType == 1);
+msns = find(cellType == 0);
+
+
+figure
+subplot(2,1,1)
+plot(bigInt(:,fsis))
+title('FSI BF Amp Responses')
+subplot(2,1,2)
+plot(bigInt(:,msns))
+title('MSN BF Amp Responses')
+
+figure
+hold on
+plot(mean(bigInt(:,fsis)'),'r')
+plot(mean(bigInt(:,msns)'),'k')
+plot(mean(bigIntLaser(:,msns)'),'g')
+plot(mean(bigIntLaser(:,fsis)'),'m')
+title('Mean Amp Resp MSNk FSIr')
+
+figure
+hold on
+plot(mean(bigIntLaser(:,fsis)')-mean(bigInt(:,fsis)'),'r')
+plot(mean(bigIntLaser(:,msns)')-mean(bigInt(:,msns)'),'k')
+title('Mean Diff Amp Resp MSNk FSIr')
+
+figure
+hold on
+for i = 1:length(fsis)
+    plot(bigInt(:,fsis(i)),bigIntLaser(:,fsis(i)),'k.-')
+end
+maxVal = max(max(bigIntLaser(:,fsis)));
+plot([0 maxVal],[0 maxVal],'r')
+
+figure
+hold on
+for i = 1:length(msns)
+    plot(bigInt(:,msns(i)),bigIntLaser(:,msns(i)),'k.-')
+end
+maxVal = max(max(bigIntLaser(:,msns)));
+plot([0 maxVal],[0 maxVal],'r')
+
+%lets look at average effects for FSIs
+
+% [b,bintr,bintjm] = gmregress(valStore(:,1),valStore(:,2),sigVal);
+sigVal = 0.05;
+counter = 1;
+slopeStore = [];
+intStore = [];
+valueStore=[];
 for i = 1:length(fsis)
     [b,bintr,bintjm] = gmregress(bigInt(:,fsis(i)),bigIntLaser(:,fsis(i)),sigVal);
     %lets also store the values.
@@ -119,6 +292,7 @@ sigVal = 0.05;
 counter = 1;
 slopeStore = [];
 intStore = [];
+valueStore=[];
 for i = 1:length(msns)
     [b,bintr,bintjm] = gmregress(bigInt(:,msns(i)),bigIntLaser(:,msns(i)),sigVal);
     %lets also store the values.
@@ -136,10 +310,6 @@ avInt = nanmean(intStore);
 [b,bintr,bintjm] = gmregress(valueStore(:,1),valueStore(:,2),sigVal);
 
 
-
-
-
-clear
 
 %% now lets look at some individual files. 
 load('181114_ML180820I_L_AudStr_pen2_3320_3mWPVHaloTuningAltLaserFullTuningAnalysis (1).mat')
