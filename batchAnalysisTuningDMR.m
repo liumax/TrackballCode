c67b7356c39fc393a1b8dcd1a02f06fbbda08d33
Max Liu <liumax@gmail.com>
2019-02-13 12:17:37 -0800

    batchAnalysisTuningDMR: batch code for running repeated analyses. Working.

---
+++ batchAnalysisTuningDMR.m
@@ -0,0 +1,39 @@
+%this is meant to be master code to run analysis code!
+%Establishes the home folder as the base
+masterFolder = pwd;
+masterDir = dir;
+masterDir = {masterDir.name};
+%this code here is to remove folders reported in dir like "." or ".."
+masterIndex = strfind(masterDir,'ML');
+masterIndex = find(not(cellfun('isempty', masterIndex)));
+
+
+%masterFolders is a listing of all folders that I want to target for batch
+%analysis
+masterFolders = masterDir(masterIndex);
+
+[findString] = functionCellStringFind(masterFolders,'.pdf');
+masterFolders(findString) = [];
+
+
+
+numFolders = length(masterFolders);
+for masterCount = 1:numFolders
+    %generates text that signifies path to the target folder
+    targetPath = strcat(masterFolder,'\',masterFolders{masterCount});
+    cd(targetPath)
+
+    fileName = masterFolders{masterCount};
+%     fileName = fileName(1:end-4);
+    
+%     periodFinder = strfind(fileName,'.');
+%     fileName = fileName(1:periodFinder-1);
+    try
+        testerAnalysisTuningAndDMR;
+    catch
+        disp('DIO FUCKUP')
+    end
+    close all
+    clearvars -except masterCount masterFolders masterFolder numFolders
+    cd(masterFolder)
+end
