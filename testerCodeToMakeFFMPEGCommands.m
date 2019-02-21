8102b715f6c3252c2e54cb0904658625259d8940
DDWSCD06706\KreitzerLab <liumax@gmail.com>
2019-02-21 11:29:55 -0800

    codeToMakeFFMPEGCommands: adjusted for generate FFMPEG

--- testerCodeToMakeFFMPEGCommands.m
+++ testerCodeToMakeFFMPEGCommands.m
@@ -2,7 +2,27 @@
 
 % bigString = [];
 strInd = 1;
-for i = 28:38
+for i = 39:51
+    if i < 10
+        newStr = strcat('ffmpeg -i "Trial',{'     '},num2str(i),'.mpg" -filter:v "crop=611:656:55:150" Trial',num2str(i),'Arena1.mp4 & ffmpeg -i "Trial',{'     '},num2str(i),'.mpg" -filter:v "crop=611:656:666:150" Trial',num2str(i),'Arena2.mp4 & ');
+%         newStr = newStr{1};
+    elseif i >= 10
+        newStr = strcat('ffmpeg -i "Trial',{'    '},num2str(i),'.mpg" -filter:v "crop=611:656:55:150" Trial',num2str(i),'Arena1.mp4 & ffmpeg -i "Trial',{'    '},num2str(i),'.mpg" -filter:v "crop=611:656:666:150" Trial',num2str(i),'Arena2.mp4 & ');
+%         newStr = newStr{1};
+    end
+    newStr = newStr{1};
+    newLength = length(newStr);
+    bigString(strInd:strInd+newLength-1) = newStr;
+    strInd = strInd + newLength;
+end
+
+
+
+
+
+% bigString = [];
+strInd = 1;
+for i = 52:63
     if i < 10
         newStr = strcat('ffmpeg -i "Trial',{'     '},num2str(i),'.mpg" -filter:v "crop=611:656:55:150" Trial',num2str(i),'Arena1.mp4 & ffmpeg -i "Trial',{'     '},num2str(i),'.mpg" -filter:v "crop=611:656:666:150" Trial',num2str(i),'Arena2.mp4 & ');
 %         newStr = newStr{1};
